55bc26bd516670bf18a7eeee84cabfb94cee532f
[anon4445] <[tahrim.anon21@gmail.com]>
2025-03-29 13:18:26 +0600

    update fuction.py

--- function.py
+++ function.py
@@ -1,3 +1,12 @@
 def combine_words(first, last):
     combined_words = first + " " + last
     return combined_words
+
+first_word = "something"
+last_word = "else"
+result = combine_words(first_word, last_word)
+print(result)
+
+first_word = "nothing"
+result = combine_words(first_word, last_word)
+print(result)
